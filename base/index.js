5b665207f13731b7c4ed31a833a3bfabf3d7088e
Zoid <zoid@zoid.one>
2024-12-21 00:30:45 -0600

    holy shit v1 of the site??

---
+++ base/index.js
@@ -0,0 +1,89 @@
+document.addEventListener("DOMContentLoaded", function () {
+    window.particlesJS.load("particles-js", "particles.json", function () {
+        console.log("callback - particles.js config loaded");
+    });
+});
+
+// https://codepen.io/soulwire/pen/mErPAK
+class TextScramble {
+    constructor(el) {
+        this.el = el;
+        this.chars = "!<>-_\\/[]{}—=+*^?#________";
+        this.update = this.update.bind(this);
+    }
+    setText(newText) {
+        const oldText = this.el.innerText;
+        const length = Math.max(oldText.length, newText.length);
+        const promise = new Promise((resolve) => (this.resolve = resolve));
+        this.queue = [];
+        for (let i = 0; i < length; i++) {
+            const from = oldText[i] || "";
+            const to = newText[i] || "";
+            const start = Math.floor(Math.random() * 40);
+            const end = start + Math.floor(Math.random() * 40);
+            this.queue.push({ from, to, start, end });
+        }
+        cancelAnimationFrame(this.frameRequest);
+        this.frame = 0;
+        this.update();
+        return promise;
+    }
+    update() {
+        let output = "";
+        let complete = 0;
+        for (let i = 0, n = this.queue.length; i < n; i++) {
+            let { from, to, start, end, char } = this.queue[i];
+            if (this.frame >= end) {
+                complete++;
+                output += to;
+            } else if (this.frame >= start) {
+                if (!char || Math.random() < 0.28) {
+                    char = this.randomChar();
+                    this.queue[i].char = char;
+                }
+                output += `<span class="dud">${char}</span>`;
+            } else {
+                output += from;
+            }
+        }
+        this.el.innerHTML = output;
+        if (complete === this.queue.length) {
+            this.resolve();
+        } else {
+            this.frameRequest = requestAnimationFrame(this.update);
+            this.frame++;
+        }
+    }
+    randomChar() {
+        return this.chars[Math.floor(Math.random() * this.chars.length)];
+    }
+}
+
+const phrases = [
+    "a minecraft modder",
+    "a software dev",
+    "an expert procrastinator",
+    "a discord user",
+    "a web developer??",
+    "a gamer",
+    "a professional idiot",
+];
+
+document.addEventListener("DOMContentLoaded", function () {
+    const el = document.getElementById("status");
+    if (el) {
+        const fx = new TextScramble(el);
+
+        let counter = 0;
+        const next = () => {
+            fx.setText(phrases[counter]).then(() => {
+                setTimeout(next, 1000);
+            });
+            counter = (counter + 1) % phrases.length;
+        };
+
+        next();
+    } else {
+        console.error("Element with id 'name' not found.");
+    }
+});
